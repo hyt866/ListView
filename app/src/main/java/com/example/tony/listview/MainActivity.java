06633ec67e309cbe466f0c28b0223d177d864def
hyt866 <hyt866@gmail.com>
2016-09-14 19:18:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tony/listview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.tony.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"dfasdf","dsfdsafef","dfewfdf","erwqefds","erewfdsaf","fddsfdsaf"};
+
+        ListAdapter theAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
